ef68843c3ccad957e90a9ced0a0abe25603a73b9
Matthew Gaston <matthew.gaston@broadcom.com>
2024-03-23 17:49:42 -0400

    lightweight app

--- Program.cs
+++ Program.cs
@@ -13,17 +13,64 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapGet("/todoitems", async (TodoContext db) =>
+    await db.TodoItems.ToListAsync());
+
+app.MapGet("/todoitems/complete", async (TodoContext db) =>
+    await db.TodoItems.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoContext db) =>
+    await db.TodoItems.FindAsync(id)
+        is TodoItem todo
+            ? Results.Ok(todo)
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (TodoItem todo, TodoContext db) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    db.TodoItems.Add(todo);
+    await db.SaveChangesAsync();
 
-app.UseHttpsRedirection();
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
 
-app.UseAuthorization();
+app.MapPut("/todoitems/{id}", async (int id, TodoItem inputTodo, TodoContext db) =>
+{
+    var todo = await db.TodoItems.FindAsync(id);
 
-app.MapControllers();
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoContext db) =>
+{
+    if (await db.TodoItems.FindAsync(id) is TodoItem todo)
+    {
+        db.TodoItems.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    return Results.NotFound();
+});
+
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.MapControllers();
 
 app.Run();
